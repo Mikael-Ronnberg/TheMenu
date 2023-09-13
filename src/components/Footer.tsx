a8ee46c6ced769ee0973165e5ce8476c9a6fd63d
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-09-13 23:55:12 +0200

    Build: added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <>
+      <ul>
+        <li>
+          <p>Content here</p>
+        </li>
+        <li>
+          <p>Content here</p>
+        </li>
+        <li>
+          <p>Content here</p>
+        </li>
+      </ul>
+    </>
+  );
+}
