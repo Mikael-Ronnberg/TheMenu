b7abdcb1dcd14fc325cca9c7b832b04bb647f95d
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-09-17 20:32:39 +0200

    Build: gave landingpage some content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,20 +11,26 @@ export default function Home() {
         <section className="hero_image"></section>
         <section className="information_container">
           <div className="imgtext_container">
+            <div className="img1"></div>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Assumenda ad quam repudiandae mollitia, voluptatem, similique eos,
               ipsa architecto commodi totam nobis esse quisquam nulla vero quas
               enim perferendis laborum ducimus?
             </p>
+          </div>
           <div className="imgtext_container">
+            <div className="imgtex2"></div>
+
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Assumenda ad quam repudiandae mollitia, voluptatem, similique eos,
               ipsa architecto commodi totam nobis esse quisquam nulla vero quas
               enim perferendis laborum ducimus?
             </p>
+          </div>
           <div className="imgtext_container">
+            <div className="img3"></div>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Assumenda ad quam repudiandae mollitia, voluptatem, similique eos,
