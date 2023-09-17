e0a4d374f23722728d3954e08c5b6f655bf25316
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-09-17 20:20:20 +0200

    Style: changed color on navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,30 @@ export default function Home() {
       <Navbar />
       <main className="md:content-center items-center w-full h-screen">
         <section className="hero_image"></section>
+        <section className="information_container">
+          <div className="imgtext_container">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Assumenda ad quam repudiandae mollitia, voluptatem, similique eos,
+              ipsa architecto commodi totam nobis esse quisquam nulla vero quas
+              enim perferendis laborum ducimus?
+            </p>
+          <div className="imgtext_container">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Assumenda ad quam repudiandae mollitia, voluptatem, similique eos,
+              ipsa architecto commodi totam nobis esse quisquam nulla vero quas
+              enim perferendis laborum ducimus?
+            </p>
+          <div className="imgtext_container">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Assumenda ad quam repudiandae mollitia, voluptatem, similique eos,
+              ipsa architecto commodi totam nobis esse quisquam nulla vero quas
+              enim perferendis laborum ducimus?
+            </p>
+          </div>
+        </section>
       </main>
 
       <Footer />
